7410531f23c1c0e5204225551715eda83727cda9
NISCHITH <NISCHITH@DESKTOP-QS8U5QG>
2026-04-11 12:17:12 +0530

    This is my second commit

--- src/test/java/demo/Practise.java
+++ src/test/java/demo/Practise.java
@@ -4,7 +4,8 @@ public class Practise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			System.out.println("hello selenium");
+			System.out.println("nischi");
 	}
 
 }
